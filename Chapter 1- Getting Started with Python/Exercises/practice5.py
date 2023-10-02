b831977e651bd4d5a63aef5e1c404d339e18e5fd
Angelyuo <lovenovene@gmail.com>
2023-10-02 12:12:33 +0400

    practice5.py
    Marc Angel B. Aparicio

---
+++ Chapter 1- Getting Started with Python/Exercises/practice5.py
@@ -0,0 +1,13 @@
+print("""Repeat after me!
+      """)
+while True:
+    i = input("I... ")
+    if i.upper() == "I":
+        l = input("Love... ")
+        if l.upper() == "LOVE":
+            b = input("Bath... ")
+            if b.upper() == "BATH":
+                s = input("Spa... ")
+                if s.upper() == "SPA":
+                    print(i+l+b+s)
+                    break
