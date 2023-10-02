afcff9ac0917f18382ade16399bf2bd81a484c8b
Angelyuo <lovenovene@gmail.com>
2023-10-02 12:11:39 +0400

    practice3.py
    Marc Angel B. Aparicio

---
+++ Chapter 1- Getting Started with Python/Exercises/practice3.py
@@ -0,0 +1,5 @@
+print("NUMBER LEADERBOARD")
+count = 1
+while count < 10: # Process will repeat until the condition is false
+    print(str(count)+".")
+    count = count + 1 # The count will increase by 1 in each line until 9
