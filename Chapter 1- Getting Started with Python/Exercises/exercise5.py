85ee2090030f9c0091b3afa7210340e65ec7c0a0
Angelyuo <lovenovene@gmail.com>
2023-10-02 09:42:39 +0400

    Exercise5 (update)
    Marc Angel B. Aparicio

--- Chapter 1- Getting Started with Python/Exercises/exercise5.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise5.py
@@ -1,8 +1,18 @@
+import math
 print("""
 AREA OF A CIRCLE CALCULATOR
 """)
-
-radius = float(input("Input the radius: "))
-answer = radius ** 2 * 3.14
-
-print(answer)
+while True:
+    rd = input("Do you want to input RADIUS or DIAMETER? ")
+    if rd.upper() == "RADIUS" or "R":
+        radius = float(input("Input the RADIUS: "))
+        answer = radius ** 2 * math.pi
+        print("The area of the circle is:",answer)
+        break
+    elif rd.upper() == "DIAMETER" or "D":
+        diameter = float(input("Input the DIAMETER: "))
+        answer = (diameter/2) ** 2 * math.pi
+        print("The area of the circle is:",answer)
+        break
+    else:
+        print("False input, try again!")
