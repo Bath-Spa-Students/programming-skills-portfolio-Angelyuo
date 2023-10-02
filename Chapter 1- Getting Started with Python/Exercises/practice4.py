2a13577a6794508f3e0c54ac037fd800dff7fe2b
Angelyuo <lovenovene@gmail.com>
2023-10-02 12:12:18 +0400

    practice4.py
    Marc Angel B. Aparicio

---
+++ Chapter 1- Getting Started with Python/Exercises/practice4.py
@@ -0,0 +1,8 @@
+print("""AREA OF A TRIANGLE CALCULATOR
+      """)
+# Title of the process
+base = float(input("Input BASE: "))
+height = float(input("Input HEIGHT: "))
+formula = (base*height)/2
+# Calculation
+print("The area of the triangle is",formula)
