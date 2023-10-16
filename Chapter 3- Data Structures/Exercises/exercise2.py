5e84416ba7b5bc58d234b7cfc507a6f895dabd4c
Angelyuo <lovenovene@gmail.com>
2023-10-16 12:03:14 +0400

    exercise2.py (updated)
    Marc Angel B. Aparicio

--- Chapter 3- Data Structures/Exercises/exercise2.py
+++ Chapter 3- Data Structures/Exercises/exercise2.py
@@ -4,3 +4,9 @@ print(f"Hi {names[1]}! :3")
 print(f"Hi {names[2]}! :3")
 print(f"Hi {names[3]}! :3")
 print(f"Hi {names[4]}! :3")
+
+"""Start with the list you used in Exercise 1, but instead of just
+
+printing each person’s name, print a message to them. The text of each message should be the same, but each message should be 
+
+personalized with the person’s name."""
