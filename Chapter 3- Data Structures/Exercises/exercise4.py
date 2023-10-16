c1d70ec4f5968abde71b76ca6309504c87c60423
Angelyuo <lovenovene@gmail.com>
2023-10-16 12:04:36 +0400

    exercise4.py (updated)
    Marc Angel B. Aparicio

--- Chapter 3- Data Structures/Exercises/exercise4.py
+++ Chapter 3- Data Structures/Exercises/exercise4.py
@@ -1,4 +1,5 @@
-family = ["Lola","Lolo","Mama","Papa"]
-
+family = ["Lola","Lolo","Mama","Papa"] 
+# If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner.
 for family in family:
     print(f"Hello {family}! I invite you to come to dinner! See you there :)")
+# Then use your list to print a message to each person, inviting them to dinner.
