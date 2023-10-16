e755a8a7a623462f80fdfa7a02ab5e65b36c97f5
Angelyuo <lovenovene@gmail.com>
2023-10-16 12:00:01 +0400

    exercise5.py
    Marc Angel B. Aparicio

---
+++ Chapter 3- Data Structures/Exercises/exercise5.py
@@ -0,0 +1,17 @@
+family = ["Lola","Lolo","Mama","Papa"]
+    
+for family in family:
+    print(f"Hello {family}! I invite you to come to dinner! See you there :)")
+
+"""You just heard that one of your guests can’t make the
+dinner, so you need to send out a new set of invitations. You’ll have to think of
+someone else to invite."""
+
+family = ["Lola","Lolo","Mama","Papa"]
+print(f"It seems that {family[0]} is not able to come, how unfortunate. Guess I will invite another person.") 
+# Add a print() call at the end of your program stating the name of the guest who can’t make it.
+
+
+family[0] = "Angelo"
+for family in family:
+    print(f"Hello {family}! I invite you to come to dinner! See you there :)")
