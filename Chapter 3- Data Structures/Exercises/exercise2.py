0ab1737d53cd5ff0aee7351c0db48345db266ccb
Angelyuo <lovenovene@gmail.com>
2023-10-16 11:59:05 +0400

    exercise2.py
    Marc Angel B. Aparicio

---
+++ Chapter 3- Data Structures/Exercises/exercise2.py
@@ -0,0 +1,6 @@
+names = ["Whyenn","Rei","Berry","Hayden","Marsh"]
+print(f"Hi {names[0]}! :3")
+print(f"Hi {names[1]}! :3")
+print(f"Hi {names[2]}! :3")
+print(f"Hi {names[3]}! :3")
+print(f"Hi {names[4]}! :3")
