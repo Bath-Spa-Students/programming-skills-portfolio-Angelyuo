144c9d6c4820703ea32e3904dfc3a1768babfbf5
Angelyuo <lovenovene@gmail.com>
2023-10-23 19:51:10 +0400

    exercise3.py
    Marc Angel B. Aparicio

---
+++ Chapter 4- Control Flow/Exercises/exercise3.py
@@ -0,0 +1,47 @@
+# Turn your if-else chain from Exercise 5-4 into an if-elifelse chain.
+
+alien_color = "green" # If the alien is green, print a message that the player earned 5 points.
+
+if alien_color == "green":
+    print("You just earned 5 points!")
+elif alien_color == "yellow":
+    print("You just earned 10 points!!")
+elif alien_color == "red":
+    print("You just earned 15 points!!!")
+else:
+    print("I don't think we have any aliens of that color...")
+
+alien_color = "yellow" # If the alien is yellow, print a message that the player earned 10 points.
+
+if alien_color == "green":
+    print("You just earned 5 points!")
+elif alien_color == "yellow":
+    print("You just earned 10 points!!")
+elif alien_color == "red":
+    print("You just earned 15 points!!!")
+else:
+    print("I don't think we have any aliens of that color...")
+
+alien_color = "red" # If the alien is red, print a message that the player earned 15 points.
+
+if alien_color == "green":
+    print("You just earned 5 points!")
+elif alien_color == "yellow":
+    print("You just earned 10 points!!")
+elif alien_color == "red":
+    print("You just earned 15 points!!!")
+else:
+    print("I don't think we have any aliens of that color...")
+
+alien_color = "blue"
+
+if alien_color == "green":
+    print("You just earned 5 points!")
+elif alien_color == "yellow":
+    print("You just earned 10 points!!")
+elif alien_color == "red":
+    print("You just earned 15 points!!!")
+else:
+    print("I don't think we have any aliens of that color...")
+
+# Write three versions of this program, making sure each message is printed for the appropriate color alien.
