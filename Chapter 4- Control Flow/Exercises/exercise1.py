c860c0773594acf2ef8dd8c364b49f093f1e2a26
Angelyuo <lovenovene@gmail.com>
2023-10-23 19:50:31 +0400

    exercise1.py
    Marc Angel B. Aparicio

---
+++ Chapter 4- Control Flow/Exercises/exercise1.py
@@ -0,0 +1,12 @@
+# Create a variable called alien_color and assign it a value of 'green'
+alien_color = "green"
+
+# Write an if statement to test whether the alien’s color is green.
+
+# Write one version of this program that passes the if test and another that fails.
+if alien_color == "green":
+    print("You just earned 5 points!")
+    # If it is, print a message that the player just earned 5 points.
+if alien_color == "red":
+    print("You just earned 5 points!")
+    # The version that fails will have no output.
