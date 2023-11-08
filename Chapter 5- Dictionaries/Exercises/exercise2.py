ac8fe11874030af7199048d33fdfadaee0734ba0
Angelyuo <lovenovene@gmail.com>
2023-11-08 11:26:39 +0400

    exercise2.py
    Marc Angel B. Aparicio

---
+++ Chapter 5- Dictionaries/Exercises/exercise2.py
@@ -0,0 +1,14 @@
+code = {
+'if/elif/else':'If statements that does specific commands if it matches a criteria',
+'variables':'Stores values',
+'input':"Receives a user's input",
+'block':'A group of statements together',
+'print':'Displays a message'
+}
+# Think of five programming words you’ve learned about in the previous chapters.
+print(f"Variables: \n{code['variables']}\n")
+print(f"Input: \n{code['input']}\n")
+print(f"Block: \n{code['block']}\n")
+print(f"Print: \n{code['print']}\n")
+# Print each word and its meaning as neatly formatted output.
+# Use the newline character (\n) to insert a blank line between each word-meaning pair in your output.
