d50094bfb59c68bcae5e19eef0a97b45ec90552b
Angelyuo <lovenovene@gmail.com>
2023-11-08 11:26:58 +0400

    exercise3.py
    Marc Angel B. Aparicio

---
+++ Chapter 5- Dictionaries/Exercises/exercise3.py
@@ -0,0 +1,26 @@
+code = {
+'if/elif/else':'If statements that does specific commands if it matches a criteria',
+'variables':'Stores values',
+'input':"Receives a user's input",
+'block':'A group of statements together',
+'print':'Displays a message'
+}
+for word, meaning in code.items():
+    print(f"{word}:\n{meaning}\n")
+# Replace your series of print() calls with a loop that runs through the dictionary’s keys and values.
+print("--------------------") # Divider
+code = {
+'if/elif/else':'If statements that does specific commands if it matches a criteria',
+'variables':'Stores values',
+'input':"Receives a user's input",
+'block':'A group of statements together',
+'print':'Displays a message',
+'integer':'Coding with whole numbers',
+'string':'Coding with text',
+'float':'Coding with decimals (and math)',
+'loop':'Code that repeats until certain conditions are met',
+'f statement':'A clean way to add variables in print()'
+}
+for word, meaning in code.items():
+    print(f"{word}:\n{meaning}\n")
+# add five more Python terms to your glossary.When you run your program again, these new words and meanings should automatically be included in the output.
