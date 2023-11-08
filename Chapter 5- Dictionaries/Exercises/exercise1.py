53911e5e830d8502a603205427c57a8b0c2f2043
Angelyuo <lovenovene@gmail.com>
2023-11-08 11:26:12 +0400

    exercise1.py
    Marc Angel B. Aparicio

---
+++ Chapter 5- Dictionaries/Exercises/exercise1.py
@@ -0,0 +1,9 @@
+id = {
+'First Name':'Ana',
+'Last Name':'Amari',
+'Age':62,
+'City':"Cairo"
+}
+# Use a dictionary to store information about a person you know.Store their first name, last name, age, and the city in which they live.
+print(id)
+# Print each piece of information stored in your dictionary.
