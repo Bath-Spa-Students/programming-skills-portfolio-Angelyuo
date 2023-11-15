b50ba4a913b988f79c0edac48a8d11781e5024d0
Angelyuo <lovenovene@gmail.com>
2023-11-15 19:14:58 +0400

    exercise1.py (updated)
    Marc Angel B. Aparicio

--- Chapter 7- Functions/Exercises/exercise1.py
+++ Chapter 7- Functions/Exercises/exercise1.py
@@ -1,3 +1,3 @@
-def display_message():
-    print("Everyone! I am learning Chapter 7 of functions!")
-display_message()
+def display_message(): # Write a function called display_message()
+    print("Everyone! I am learning Chapter 7 of functions!") # Prints one sentence telling everyone what you are learning about in this chapter.
+display_message() # Call the function, and make sure the message displays correctly.
